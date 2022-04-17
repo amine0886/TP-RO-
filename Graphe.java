94b28578f2c75b3766d3b567b935df21f9e41e0c
amine0886 <98590213+amine0886@users.noreply.github.com>
2022-04-17 11:58:33 +0100

    Update Graphe.java

--- Graphe.java
+++ Graphe.java
@@ -4,7 +4,7 @@ import ilog.cplex.*;
 
 package tp2;
 
-/*public class app {
+public class app {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -24,51 +24,5 @@ package tp2;
 	}
 
 }
-*/
-
-public class Graphe {
-    protected  int nombreSommet;
-    protected int nombreArcs;
-    protected  int matriceAdjacence[][];
-    protected  int matriceIncidence[][];
-
-    public Graphe(int nombreSommet,int nombreArcs,int matriceAdjacence[][]){
-        this.nombreSommet=nombreSommet;
-        this.nombreArcs=nombreArcs;
-        this.matriceAdjacence=matriceAdjacence;
-        this.matriceIncidence=construireMatriceIncidence(this.matriceAdjacence);
-    }
-    private int[][] construireMatriceIncidence(int[][] matriceAdjacence2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public int getNombreArcs() {
-        return nombreArcs;
-    }
-
-    public int getNombreSommet() {
-        return nombreSommet;
-    }
-    //fonction qui retourne la matrice d'adjacence
-    public int[][] getMatriceAdjacence() {
-        return this.matriceAdjacence;
-    }
-    //fonction qui retourne la matrice d'adjacence
-    public int[][] getMatriceIncidence() {
-        return this.matriceIncidence;
-    }
-    private int[][] construireMatriceIncidence1(int[][] matriceAdjacence) {
-        int matrice[][]=new int[matriceAdjacence.length][matriceAdjacence.length];
-        for (int i = 0; i < matriceAdjacence.length; i++) {
-            for (int j = 0; j <matriceAdjacence[i].length ; j++) {
-                if(matriceAdjacence[i][j]!=0){
-                    matrice[i][j]=1;
-                }else{
-                    matrice[i][j]=0;
-                }
-            }
-        }
-        return matrice;
-    }
 
    
